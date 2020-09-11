696076d35d7980bb7d5a723822b69e849445fa48
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-09-11 12:05:43 +0200

    Add QR-service

---
+++ backend/QR-service/src/main/java/com/dma/qrservice/QRServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dma.qrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class QRServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QRServiceApplication.class, args);
+    }
+}
