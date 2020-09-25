d39445ed52833bd0a68a6f6714e829f91cda52d8
Aron Hemmes <aronhemmes@live.nl>
2020-09-25 19:18:11 +0200

    Refactor frontend

--- frontend/restaurant/src/App.js
+++ frontend/restaurant/src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import {  BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
+import Home from './default/components/Home';
+import NavBar from "./default/components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import restaurant from "./restaurant/Restaurant";
+import Banner from "./restaurant/components/Banner";
+import OrderMenuCategory from "./restaurant/components/OrderMenuCategory";
+import OrderBar from "./restaurant/components/OrderBar";
+import Table from "./restaurant/components/Table";
+import OrderMenu from "./restaurant/components/OrderMenu";
+import RestaurantHome from './restaurant/components/Home';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Switch>
+                        <Route exact strict path="/">
+                            <>
+                                <NavBar/>
+                                <div className="content">
+                                    <Home/>
+                                </div>
+                            </>
+                        </Route>
+
+                        {/* customer */}
+
+                            <Route exact strict path={"/" + restaurant.getName()}>
+                                <>
+                                    <NavBar/>
+                                    <div className="content">
+                                        <Banner/>
+                                        <RestaurantHome/>
+                                    </div>
+                                </>
+                            </Route>
+
+                            <Route exact strict path={"/" + restaurant.getName() + "/" + restaurant.getTable()}>
+                                <>
+                                    <NavBar/>
+                                    <div className="content">
+                                        <Banner/>
+                                        <Table/>
+                                    </div>
+                                </>
+                            </Route>
+
+                            <Route exact strict path={"/" + restaurant.getName() + "/" + restaurant.getTable() + "/order"}>
+                                <>
+                                    <NavBar/>
+                                    <div className="content">
+                                        <Banner/>
+                                        <OrderMenu/>
+                                        <OrderBar/>
+                                    </div>
+                                </>
+                            </Route>
+
+                            {restaurant.getMenu().map(category => {
+                                console.log(category);
+                                return (
+                                    <Route exact strict path={"/" + restaurant.getName() + "/" + restaurant.getTable() + "/order/" + category}>
+                                        <>
+                                            <NavBar/>
+                                            <div className="content">
+                                                <Banner/>
+                                                <OrderMenuCategory/>
+                                                <OrderBar/>
+                                            </div>
+                                        </>
+                                    </Route>
+                                )
+                            })}
+
+                            <Route path={"/" + restaurant.getName() + "/" + restaurant.getTable() + "/*"}>
+                                <Redirect to={"/" + restaurant.getName() + "/" + restaurant.getTable()}/>
+                            </Route>
+
+                            <Route path={"/" + restaurant.getName() + "/*"}>
+                                <Redirect to={"/" + restaurant.getName()}/>
+                            </Route>
+
+
+                        <Route path="*">
+                            <Redirect to="/"/>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
