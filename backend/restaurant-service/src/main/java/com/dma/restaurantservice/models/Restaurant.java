1bf90e62e18350789fae534168e5558f6c08b412
Aron Hemmes <aronhemmes@live.nl>
2020-10-04 23:38:54 +0200

    Add tableCount to restaurant

--- backend/restaurant-service/src/main/java/com/dma/restaurantservice/models/Restaurant.java
+++ backend/restaurant-service/src/main/java/com/dma/restaurantservice/models/Restaurant.java
@@ -1,6 +1,7 @@
 package com.dma.restaurantservice.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +21,26 @@ public class Restaurant {
     @Column(unique = true)
     private String name;
 
+    @NotNull
+    @Min(1)
+    private int tableCount = 10;
+
     private String colorScheme;
 
     private String logoURL;
 
     @ElementCollection
     private List<Integer> menuIDs;
+
+    public Restaurant() {
+    }
+
+    public Restaurant(String name, String colorScheme, String logoURL, ArrayList<Integer> menuIDs) {
+        this.name = name;
+        this.colorScheme = colorScheme;
+        this.logoURL = logoURL;
+        this.menuIDs = menuIDs;
+    }
 
     public long getId() {
         return id;
@@ -67,13 +82,11 @@ public class Restaurant {
         this.menuIDs = menuIDs;
     }
 
-    public Restaurant() {
+    public int getTableCount() {
+        return tableCount;
     }
 
-    public Restaurant(String name, String colorScheme, String logoURL, ArrayList<Integer> menuIDs) {
-        this.name = name;
-        this.colorScheme = colorScheme;
-        this.logoURL = logoURL;
-        this.menuIDs = menuIDs;
+    public void setTableCount(int tableCount) {
+        this.tableCount = tableCount;
     }
 }
