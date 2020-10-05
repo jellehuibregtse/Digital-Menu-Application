7d829e2884773a3b3be28d8feb8b4eb9060a1769
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2020-10-05 14:45:11 +0200

    Add messaging controller and websocketconfig

---
+++ backend/service-registry/src/main/java/com/dmao/serviceregistry/controllers/MessagingController.java
@@ -0,0 +1,21 @@
+package com.dmao.serviceregistry.controllers;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * The websocket configuration class.
+ *
+ * @author Aron Hemmes
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/messaging")
+public class MessagingController {
+
+    public void subscribe(String URL, String Response) {
+
+    }
+}
