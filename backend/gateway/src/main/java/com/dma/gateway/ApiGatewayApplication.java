06a202b55ee56e30eca45d10a898d986e95d290e
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2020-10-05 20:27:01 +0200

    Add eureka client and author

--- backend/gateway/src/main/java/com/dma/gateway/ApiGatewayApplication.java
+++ backend/gateway/src/main/java/com/dma/gateway/ApiGatewayApplication.java
@@ -2,8 +2,15 @@ package com.dma.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * The entry point for the application.
+ *
+ * @author Aron Hemmes
+ */
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
