a2bb5c74b3489f7365f2c92587992d6616dfa143
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2020-10-05 12:39:04 +0200

    Add RestTemplate bean and allow crossorigin

--- backend/service-registry/src/main/java/com/dmao/serviceregistry/ServiceRegistryApplication.java
+++ backend/service-registry/src/main/java/com/dmao/serviceregistry/ServiceRegistryApplication.java
@@ -2,9 +2,21 @@ package com.dmao.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin
 @SpringBootApplication
 public class ServiceRegistryApplication {
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
