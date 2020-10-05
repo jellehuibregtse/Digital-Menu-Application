8521f099bee7dd58fd68f07cd311bacb73cbf97e
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2020-10-05 13:43:59 +0200

    Add methods for restaurant

--- backend/service-registry/src/main/java/com/dmao/serviceregistry/controllers/RestaurantController.java
+++ backend/service-registry/src/main/java/com/dmao/serviceregistry/controllers/RestaurantController.java
@@ -1,9 +1,10 @@
 package com.dmao.serviceregistry.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/restaurants")
 public class RestaurantController {
 
+    private final String URL = "http://restaurant-service/api/restaurants";
     private final RestTemplate restTemplate;
 
     @Autowired
@@ -18,4 +20,49 @@ public class RestaurantController {
         this.restTemplate = restTemplate;
     }
 
+    @GetMapping("/")
+    public ResponseEntity<?> getAllRestaurants() {
+        try {
+            return restTemplate.exchange(URL + "/", HttpMethod.GET, null, Object.class);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getRestaurant(@PathVariable long id) {
+        try {
+            return restTemplate.exchange(URL + "/" + id, HttpMethod.GET, null, Object.class);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+    @PutMapping("/")
+    public ResponseEntity<?> updateRestaurant(@RequestBody Object restaurant) {
+        try {
+            return restTemplate.exchange(URL + "/", HttpMethod.PUT, new HttpEntity<>(restaurant), String.class);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteRestaurant(@PathVariable long id) {
+        try {
+            return restTemplate.exchange(URL + "/" + id, HttpMethod.DELETE, new HttpEntity<>(id), String.class);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> createRestaurant(@RequestBody Object restaurant) {
+        try {
+            return restTemplate.exchange(URL + "/", HttpMethod.POST, new HttpEntity<>(restaurant), String.class);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
