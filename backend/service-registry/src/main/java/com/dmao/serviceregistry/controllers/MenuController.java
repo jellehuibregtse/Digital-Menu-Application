2dff8a4deae903425a17df38618b95353b293e5a
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2020-10-05 14:28:48 +0200

    Add methods for menu

--- backend/service-registry/src/main/java/com/dmao/serviceregistry/controllers/MenuController.java
+++ backend/service-registry/src/main/java/com/dmao/serviceregistry/controllers/MenuController.java
@@ -1,9 +1,10 @@
 package com.dmao.serviceregistry.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/menus")
 public class MenuController {
 
+    private final String URL = "http://menu-service/api/menus";
     private final RestTemplate restTemplate;
 
     @Autowired
@@ -18,4 +20,48 @@ public class MenuController {
         this.restTemplate = restTemplate;
     }
 
+    @GetMapping("/")
+    public ResponseEntity<?> getAllMenus() {
+        try {
+            return restTemplate.exchange(URL + "/", HttpMethod.GET, null, Object.class);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getMenu(@PathVariable long id) {
+        try {
+            return restTemplate.exchange(URL + "/" + id, HttpMethod.GET, new HttpEntity<>(id), Object.class);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> createMenu(@RequestBody Object menu) {
+        try {
+            return restTemplate.exchange(URL + "/", HttpMethod.POST, new HttpEntity<>(menu), String.class);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<String> updateMenu(@RequestBody Object menu) {
+        try {
+            return restTemplate.exchange(URL + "/", HttpMethod.PUT, new HttpEntity<>(menu), String.class);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteMenu(@PathVariable long id) {
+        try {
+            return restTemplate.exchange(URL + "/" + id, HttpMethod.DELETE, new HttpEntity<>(id), String.class);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
