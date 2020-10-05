7e2af9e137dcef7de58ea77ad9dd175b11501e1b
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2020-10-05 20:31:35 +0200

    Clean up discovery server

--- backend/discovery-server/src/main/java/com/dma/discoveryserver/DiscoveryServerApplication.java
+++ backend/discovery-server/src/main/java/com/dma/discoveryserver/DiscoveryServerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * The entry point for the application.
+ *
+ * @author Aron Hemmes
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
