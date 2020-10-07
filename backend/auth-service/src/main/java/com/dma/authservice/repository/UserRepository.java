56aa2ac884da1be6d64a9381623cd0144000bb6d
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-10-07 11:56:25 +0200

    Add user repository

---
+++ backend/auth-service/src/main/java/com/dma/authservice/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.dma.authservice.repository;
+
+import com.dma.authservice.auth.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * The repository for getting users from the database.
+ *
+ * @author Jelle Huibregtse
+ */
+@Repository
+public interface UserRepository extends CrudRepository<ApplicationUser, Long> {
+
+    Optional<ApplicationUser> findByUsername(String username);
+
+    @Override
+    Optional<ApplicationUser> findById(Long id);
+}
