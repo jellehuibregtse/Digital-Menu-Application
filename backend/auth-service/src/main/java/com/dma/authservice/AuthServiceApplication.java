81560538bd8d6899574f55e03f919248249f6abd
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-10-07 09:33:53 +0200

    Add comment to main class

--- backend/auth-service/src/main/java/com/dma/authservice/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/dma/authservice/AuthServiceApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * The entry point of the authentication service.
+ *
+ * @author Jelle Huibregtse
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class AuthServiceApplication {
