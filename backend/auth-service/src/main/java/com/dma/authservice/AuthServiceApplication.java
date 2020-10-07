49bdb18e6e5a934313bab64ed8827d24b6974a0e
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-10-07 10:32:04 +0200

    Add JwtConfig

--- backend/auth-service/src/main/java/com/dma/authservice/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/dma/authservice/AuthServiceApplication.java
@@ -2,6 +2,7 @@ package com.dma.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@ConfigurationPropertiesScan
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
