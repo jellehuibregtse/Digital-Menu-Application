545e9a263deed2c2119524562468bd505f9f3dc1
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-10-07 12:03:44 +0200

    Rename repository
    Signed-off-by: Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>

---
+++ backend/auth-service/src/main/java/com/dma/authservice/services/ApplicationUserService.java
@@ -0,0 +1,27 @@
+package com.dma.authservice.services;
+
+import com.dma.authservice.repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationUserService implements UserDetailsService {
+
+    private final ApplicationUserRepository applicationUserRepository;
+
+    @Autowired
+    public ApplicationUserService(ApplicationUserRepository applicationUserDao) {
+        this.applicationUserRepository = applicationUserDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return applicationUserRepository.findByUsername(username)
+                                        .orElseThrow(() -> new UsernameNotFoundException(String.format(
+                                                "Username %s not found",
+                                                username)));
+    }
+}
