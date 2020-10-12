6944d904a4b56226b474412028d016759640722f
Jelle Huibregtse <jbhuibregtse@gmail.com>
2020-10-12 15:53:25 +0200

    Add OrderServiceApplicationTests

---
+++ backend/order-service/src/test/java/com/dma/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,20 @@
+package com.dma.orderservice;
+
+import com.dma.orderservice.controllers.OrderController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class OrderServiceApplicationTests {
+
+    @Autowired
+    private OrderController orderController;
+
+    @Test
+    public void contextLoads() {
+        assertThat(orderController).isNotNull();
+    }
+}
