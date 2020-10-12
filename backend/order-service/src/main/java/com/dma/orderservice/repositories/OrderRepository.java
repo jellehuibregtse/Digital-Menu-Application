81cc9bb191a7ef08cf002c87e86e1dc9659848f0
Jelle Huibregtse <jbhuibregtse@gmail.com>
2020-10-12 11:58:10 +0200

    Update OrderRepository

--- backend/order-service/src/main/java/com/dma/orderservice/repositories/OrderRepository.java
+++ backend/order-service/src/main/java/com/dma/orderservice/repositories/OrderRepository.java
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends CrudRepository<CustomerOrder, Long> {
 
+    Iterable<CustomerOrder> findAllByRestaurantId(long restaurantId);
 }
