97dbdf47e670e292093f03de7665953fc9bf82b4
Petrişor,Rareş R.I <r.petrisor@student.fontys.nl>
2020-10-13 00:20:27 +0200

    Made page responsive and added popup once you click on order items

--- frontend/kitchen/src/components/OrderView.jsx
+++ frontend/kitchen/src/components/OrderView.jsx
@@ -1,29 +1,63 @@
 import OrderColumn from "./fragments/OrderColumn";
 import React from "react";
+import "../css/ordercolumn.css";
 
 const OrderStatus = {
-    NEW: "NEW",
-    PROCESSING: "PROCESSING",
-    COMPLETE: "COMPLETE"
-}
+  NEW: "NEW",
+  PROCESSING: "PROCESSING",
+  COMPLETE: "COMPLETE",
+};
 
 const OrderView = (props) => {
-    // Get all menu items from all open orders
-    const items = [].concat.apply([], props.orders.map((order) => { return order.items.map((item) => { item.table = order.tableNumber; return item; })}));
+  // Get all menu items from all open orders
+  const items = [].concat.apply(
+    [],
+    props.orders.map((order) => {
+      return order.items.map((item) => {
+        item.table = order.tableNumber;
+        return item;
+      });
+    })
+  );
 
-    // Dividing menu items to assigned columns
-    const newItems = items.filter(item => item.status === OrderStatus.NEW);
-    const processingItems = items.filter(item => item.status === OrderStatus.PROCESSING);
-    const completeItems = items.filter(item => item.status === OrderStatus.COMPLETE);
+  // Dividing menu items to assigned columns
+  const newItems = items.filter((item) => item.status === OrderStatus.NEW);
+  const processingItems = items.filter(
+    (item) => item.status === OrderStatus.PROCESSING
+  );
+  const completeItems = items.filter(
+    (item) => item.status === OrderStatus.COMPLETE
+  );
 
-    return(
-        <div className="orderView">
-            <div className="column"><OrderColumn name={"New Dishes"} columnType={0} items={newItems}/> </div>
-            <div className="column"><OrderColumn name={"Preparing Dishes"} columnType={0} items={processingItems}/></div>
-            <div className="column"><OrderColumn name={"Completed Dishes"} columnType={0} items={completeItems}/></div>
-            <div className="column"><OrderColumn name={"Orders"} columnType={1} items={props.orders}/></div>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-3 col-sm-6">
+          <OrderColumn name={"New Dishes"} columnType={0} items={newItems} />{" "}
         </div>
-    )
-}
+
+        <div className="col-md-3 col-sm-6">
+          <OrderColumn
+            name={"Preparing Dishes"}
+            columnType={0}
+            items={processingItems}
+          />
+        </div>
+
+        <div className="col-md-3 col-sm-6">
+          <OrderColumn
+            name={"Completed Dishes"}
+            columnType={0}
+            items={completeItems}
+          />
+        </div>
+
+        <div className="col-md-3 col-sm-6">
+          <OrderColumn name={"Orders"} columnType={1} items={props.orders} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default OrderView;
--- frontend/kitchen/src/components/fragments/OrderColumn.jsx
+++ frontend/kitchen/src/components/fragments/OrderColumn.jsx
@@ -1,63 +1,103 @@
-import React from "react";
+import { Modal } from "react-bootstrap";
+import React, { useState } from "react";
 
 const OrderColumn = (props) => {
-    // Check if valid column type
-    if(!(props.columnType >= 0 && props.columnType <= 1)) { return null; }
+  const [popShow, setPopShow] = useState(false);
 
-    // Generating dishes and orders according to column type
-    // type 0: dishes
-    // type 1: orders
-    const items = props.items.length > 0? props.items.map((item) => {
-        return (
-            <tbody>
+  const handleClose = () => {
+    setPopShow(false);
+  };
+
+  const handlePopup = () => {
+    setPopShow(true);
+  };
+
+  // Check if valid column type
+  if (!(props.columnType >= 0 && props.columnType <= 1)) {
+    return null;
+  }
+
+  // Generating dishes and orders according to column type
+  // type 0: dishes
+  // type 1: orders
+
+  const items =
+    props.items.length > 0
+      ? props.items.map((item) => {
+          console.log(item);
+          return (
+            <div>
+              <tbody
+                onClick={() => {
+                  handlePopup();
+                }}
+              >
                 <tr>
-                    {props.columnType === 0? <>
-                        <th>{item.amount}</th>
-                        <td>{item.name}</td>
-                        <td>{item.table}</td>
-                    </> : <>{item.items.length > 0? <>
-                        <th>{item.id}</th>
-                        <td>
+                  {props.columnType === 0 ? (
+                    <>
+                      <th>{item.amount}</th>
+                      <td>{item.name}</td>
+                      <td>{item.table}</td>
+                    </>
+                  ) : (
+                    <>
+                      {item.items.length > 0 ? (
+                        <>
+                          <th>{item.id}</th>
+                          <td>
                             <ul>
-                                {item.items.map(function (item) {
-                                    return <li>{item.name}</li>;
-                                })}
+                              {item.items.map(function (item) {
+                                return <li>{item.name}</li>;
+                              })}
                             </ul>
-                        </td>
-                        <td>{item.createdDateTime}</td>
-                        <td>{item.tableNumber}</td>
-                        </> : null}
-                    </>}
+                          </td>
+                          <td>{item.createdDateTime}</td>
+                          <td>{item.tableNumber}</td>
+                        </>
+                      ) : null}
+                    </>
+                  )}
                 </tr>
-            </tbody>
-        )
-    }): null;
+              </tbody>
 
-    return (
-        <>
-            <h5>{props.name}</h5>
-            <table className="table table-hover">
-                <thead className="thead-dark">
-                    <tr>
-                        {props.columnType === 0?
-                            <>
-                                <th scope="col">Amount</th>
-                                <th scope="col">Dish Name</th>
-                                <th scope="col">Table</th>
-                            </> :
-                            <>
-                                <th scope="col">Order Number</th>
-                                <th scope="col">Items</th>
-                                <th scope="col">Time</th>
-                                <th scope="col">Table</th>
-                            </>
-                        }
-                    </tr>
-                </thead>
-                {items}
-            </table>
-        </>
-    );
+              <Modal show={popShow} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Body</Modal.Body>
+                <Modal.Footer>Footer</Modal.Footer>
+              </Modal>
+            </div>
+          );
+        })
+      : null;
+
+  return (
+    <>
+      <h5 id="title-column">{props.name}</h5>
+      <table className="table table-hover">
+        <thead className="thead-dark">
+          <tr>
+            {props.columnType === 0 ? (
+              <>
+                <th scope="col">Amount</th>
+                <th scope="col">Dish Name</th>
+                <th scope="col">Table</th>
+              </>
+            ) : (
+              <>
+                <th scope="col">Order Number</th>
+                <th scope="col">Items</th>
+                <th scope="col">Time</th>
+                <th scope="col">Table</th>
+              </>
+            )}
+          </tr>
+        </thead>
+        {items}
+      </table>
+    </>
+  );
 };
 
 export default OrderColumn;
