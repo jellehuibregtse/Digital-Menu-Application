ff7e0cf988eb7a8b3fcdd6dc323a7c8df50d4970
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2020-10-12 14:57:22 +0200

    Add buttons to send fetch requests to service

--- frontend/admin-panel/src/App.jsx
+++ frontend/admin-panel/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import {Button} from "@material-ui/core";
+import messagingService from './services/MessagingService';
+
+const message = {
+    username: "Aron",
+    password: "admin"
+}
 
 function App() {
   return (
       <>
+          <Button onClick={() => messagingService.fetchHandler('POST', '/users/register', message).then((res) => console.log(res)).catch(() => {})}>REGISTER</Button>
+          <Button onClick={() => messagingService.fetchHandler('POST', '/users/login', message).then((res) => console.log(res)).catch(() => {})}>LOGIN</Button>
       </>
   );
 }
