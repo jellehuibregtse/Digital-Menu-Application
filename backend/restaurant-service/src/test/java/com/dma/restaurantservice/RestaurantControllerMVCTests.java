378385486e6547ac95b5ee745f1dd674ba473534
= <=>
2020-10-18 15:03:47 +0200

    tests fixed

--- backend/restaurant-service/src/test/java/com/dma/restaurantservice/RestaurantControllerMVCTests.java
+++ backend/restaurant-service/src/test/java/com/dma/restaurantservice/RestaurantControllerMVCTests.java
@@ -61,31 +61,39 @@ public class RestaurantControllerMVCTests {
 
     @Test
     public void getAllRestaurants_andReturnStatus200() throws Exception {
+
+        Restaurant restaurantOne = repository.findByName("Test1").orElseThrow();
+        Restaurant restaurantTwo = repository.findByName("Test2").orElseThrow();
+
         this.mockMvc.perform(MockMvcRequestBuilders.get("/restaurants/"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].id", is(1)))
-                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[0].id", is((int) restaurantOne.getId())))
                 .andExpect(jsonPath("$[0].name", is("Test1")))
+                .andExpect(jsonPath("$[1].id", is((int) restaurantTwo.getId())))
                 .andExpect(jsonPath("$[1].name", is("Test2")));
 
     }
 
     @Test
     public void getRestaurantById_andReturnStatus200() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/restaurants/1"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(1)))
-                .andExpect(jsonPath("$.name", is("Test1")));
+        Restaurant restaurantOne = repository.findByName("Test1").orElseThrow();
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/restaurants/2"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/restaurants/{id}", restaurantOne.getId()))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(2)))
-                .andExpect(jsonPath("$.name", is("Test2")));
+                .andExpect(jsonPath("$.id", is((int) restaurantOne.getId())))
+                .andExpect(jsonPath("$.name", is(restaurantOne.getName())));
+
+
+        Restaurant restaurantTwo = repository.findByName("Test2").orElseThrow();
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/restaurants/{id}", restaurantTwo.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is((int) restaurantTwo.getId())))
+                .andExpect(jsonPath("$.name", is(restaurantTwo.getName())));
 
     }
 
@@ -101,32 +109,36 @@ public class RestaurantControllerMVCTests {
         Restaurant restaurant = new Restaurant("Test3", "test_theme", "test_url", new ArrayList<>());
         this.mockMvc.perform(MockMvcRequestBuilders
                 .post("/restaurants/")
-                .contentType(MediaType.APPLICATION_JSON )
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(restaurant)))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$",is(String.format("Restaurant with name: %s has been successfully created!",restaurant.getName()))));
+                .andExpect(jsonPath("$", is(String.format("Restaurant with name: %s has been successfully created!", restaurant.getName()))));
     }
+
     @Test
     public void updateRestaurant_andReturnStatus200() throws Exception {
+        Restaurant foundRestaurant = repository.findByName("Test1").orElseThrow();
+
         Restaurant restaurant = new Restaurant("UpdatedTest2", "test_theme", "test_url", new ArrayList<>());
-        restaurant.setId(2L);
+        restaurant.setId(foundRestaurant.getId());
+
         this.mockMvc.perform(MockMvcRequestBuilders
                 .put("/restaurants/")
-                .contentType(MediaType.APPLICATION_JSON )
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(restaurant)))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$",is(String.format("Restaurant with id: %d has been successfully updated!",restaurant.getId()))));
+                .andExpect(jsonPath("$", is(String.format("Restaurant with id: %d has been successfully updated!", foundRestaurant.getId()))));
     }
 
     @Test
     public void updateUnknownRestaurant_andReturnStatus404() throws Exception {
         Restaurant restaurant = new Restaurant("UpdatedTest2", "test_theme", "test_url", new ArrayList<>());
-        restaurant.setId(10L);
+        restaurant.setId(111L);
         this.mockMvc.perform(MockMvcRequestBuilders
                 .put("/restaurants/")
-                .contentType(MediaType.APPLICATION_JSON )
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(restaurant)))
                 .andDo(print())
                 .andExpect(status().isNotFound());
@@ -134,17 +146,17 @@ public class RestaurantControllerMVCTests {
 
     @Test
     public void deleteRestaurant_andReturnStatus200() throws Exception {
+        Restaurant restaurantOne = repository.findByName("Test1").orElseThrow();
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .delete("/restaurants/1"))
+                .delete("/restaurants/{id}", restaurantOne.getId()))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$",is("Restaurant with id: 1 has been successfully deleted!")));
+                .andExpect(jsonPath("$", is(String.format("Restaurant with id: %d has been successfully deleted!", restaurantOne.getId()))));
     }
 
     @Test
     public void deleteUnknownRestaurant_andReturnStatus404() throws Exception {
-
         this.mockMvc.perform(MockMvcRequestBuilders
                 .delete("/restaurants/222"))
                 .andDo(print())
