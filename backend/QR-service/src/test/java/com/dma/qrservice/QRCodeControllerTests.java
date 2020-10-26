d3bb7c8bf4dbe789bd0836d7a5fc5d0b70fab7c5
Jelle Huibregtse <jbhuibregtse@gmail.com>
2020-10-26 09:25:31 +0100

    Remove RunWith annotation

--- backend/QR-service/src/test/java/com/dma/qrservice/QRCodeControllerTests.java
+++ backend/QR-service/src/test/java/com/dma/qrservice/QRCodeControllerTests.java
@@ -1,12 +1,10 @@
 package com.dma.qrservice;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.nio.charset.StandardCharsets;
@@ -19,7 +17,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  *
  * @author Jelle Huibregtse
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class QRCodeControllerTests {
