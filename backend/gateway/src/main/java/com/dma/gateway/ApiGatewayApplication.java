d4dea283ce032bfb2ed08e9e4fab57a97d499662
Jelle Huibregtse <jbhuibregtse@gmail.com>
2020-11-05 19:11:11 +0100

    Remove GatewayConfig and make application Zuul proxy

--- backend/gateway/src/main/java/com/dma/gateway/ApiGatewayApplication.java
+++ backend/gateway/src/main/java/com/dma/gateway/ApiGatewayApplication.java
@@ -3,12 +3,15 @@ package com.dma.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * The entry point for the application.
  *
  * @author Aron Hemmes
+ * @author Jelle Huibregtse 
  */
+@EnableZuulProxy
 @SpringBootApplication
 @EnableEurekaClient
 public class ApiGatewayApplication {
