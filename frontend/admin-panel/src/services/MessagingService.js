1bb47c6f1a4d21ca69bfecbf7f036c289e76e4ac
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2020-11-12 13:14:37 +0100

    Update MessagingService

--- frontend/admin-panel/src/services/MessagingService.js
+++ frontend/admin-panel/src/services/MessagingService.js
@@ -1,19 +1,33 @@
-// Service Registry Port: 8080
+// Service Registry Port: 8762
 
 class MessagingService {
     // This is where you can get/post/put/delete messages
     static async fetchHandler(method, route, message) {
         let result = null;
-        await fetch('http://localhost:8080/api' + route, {
+        await fetch('http://localhost:8762/api' + route, {
             method: method,
             body: JSON.stringify(message),
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-            .then((response) => {if (response.ok) { return response.text() } else { throw new Error(response.statusText) }})
-            .then((res) => {try {result = typeof JSON.parse(res) === "object" && JSON.parse(res) !== null? JSON.parse(res) : res} catch (e) {result = res}})
-            .catch(error => {throw new Error(error.message)});
+            .then((response) => {
+                if (response.ok) {
+                    return response.text()
+                } else {
+                    throw new Error(response.statusText)
+                }
+            })
+            .then((res) => {
+                try {
+                    result = typeof JSON.parse(res) === "object" && JSON.parse(res) !== null ? JSON.parse(res) : res
+                } catch (e) {
+                    result = res
+                }
+            })
+            .catch(error => {
+                throw new Error(error.message)
+            });
         return result
     }
 }
