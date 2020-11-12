9d6cc1e5f329cba589cc008b8104fdba96cc0bda
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2020-11-12 13:31:44 +0100

    Create searchbar

---
+++ frontend/admin-panel/src/components/menu/Categories.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Typography} from "@material-ui/core";
+
+const Categories = () => {
+    return (
+        <>
+            <Typography variant="h4">
+                Categories
+            </Typography>
+        </>
+    )
+}
+
+export default Categories;
