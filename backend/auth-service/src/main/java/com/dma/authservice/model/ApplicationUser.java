432ca1fa20e17699bdfad1681e151838395b4f7d
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-11-13 10:55:21 +0100

    Update ApplicationUser to conform with Lombok
    Also updated the fields.

--- backend/auth-service/src/main/java/com/dma/authservice/model/ApplicationUser.java
+++ backend/auth-service/src/main/java/com/dma/authservice/model/ApplicationUser.java
@@ -1,64 +1,32 @@
 package com.dma.authservice.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Map;
 
 /**
  * The application user model.
  *
  * @author Jelle Huibregtse
  */
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class ApplicationUser {
 
     @Id
+    @Setter(AccessLevel.PROTECTED)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private @NotNull String email;
+    private @NotNull String password;
     @NotNull
-    private String username, password;
-    private String role;
-
-    public ApplicationUser(Integer id, String username, String password, String role) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.role = role;
-    }
-
-    public ApplicationUser() {}
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+    @ElementCollection
+    private Map<Long, String> restaurantAuthorities;
 }
