452f25465cea614ec8bee88b077bb7f227531f95
= <=>
2020-11-27 11:49:06 +0100

    runner fixed

--- backend/menu-service/src/main/java/com/dma/menuservice/Runner.java
+++ backend/menu-service/src/main/java/com/dma/menuservice/Runner.java
@@ -23,7 +23,7 @@ public class Runner implements CommandLineRunner {
         //Init Restaurant
         Restaurant testRestaurant = new Restaurant();
 
-        testRestaurant.setName("Test Restaurant 1");
+        testRestaurant.setName("Demo Restaurant");
         testRestaurant.setId(1L);
 
         //Init Categories
@@ -49,19 +49,27 @@ public class Runner implements CommandLineRunner {
         //Init Menu Items
         MenuItem testItemOne = new MenuItem();
         testItemOne.setName("Steak");
+        testItemOne.setPrice(12.99);
         testItemOne.setCategory(testMenu.getCategories().get(0));
         testItemOne.setIngredients(Arrays.asList(testMenu.getIngredients().get(0)));
 
         MenuItem testItemTwo = new MenuItem();
-        testItemTwo.setName("Coca Cola 330ml");
+        testItemTwo.setName("Fanta 330ml");
+        testItemTwo.setPrice(1.99);
         testItemTwo.setCategory(testMenu.getCategories().get(1));
 
         MenuItem testItemThree = new MenuItem();
         testItemThree.setName("Salmon with fries");
+        testItemThree.setPrice(15.99);
         testItemThree.setCategory(testMenu.getCategories().get(0));
         testItemThree.setIngredients(Arrays.asList(testMenu.getIngredients().get(1)));
 
-        List<MenuItem> testItems = Arrays.asList(testItemOne, testItemTwo,testItemThree);
+        MenuItem testItemFour = new MenuItem();
+        testItemFour.setName("Lipton 330ml");
+        testItemFour.setPrice(1.99);
+        testItemFour.setCategory(testMenu.getCategories().get(1));
+
+        List<MenuItem> testItems = Arrays.asList(testItemOne, testItemTwo,testItemThree,testItemFour);
         testMenu.setItems(testItems);
 
         repository.save(testMenu);
