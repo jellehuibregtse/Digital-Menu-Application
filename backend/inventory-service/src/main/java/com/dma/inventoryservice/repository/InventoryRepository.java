78edbbe9f5c4ddd8d5fc3fb1c7688fffbcee5151
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-12-11 15:14:51 +0100

    Add repository annotation to inventory repository

--- backend/inventory-service/src/main/java/com/dma/inventoryservice/repository/InventoryRepository.java
+++ backend/inventory-service/src/main/java/com/dma/inventoryservice/repository/InventoryRepository.java
@@ -3,8 +3,11 @@ package com.dma.inventoryservice.repository;
 import com.dma.inventoryservice.model.Inventory;
 import com.dma.inventoryservice.model.InventoryItem;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface InventoryRepository extends CrudRepository<Inventory, Long> {
 
     Optional<InventoryItem> findByName(String name);
