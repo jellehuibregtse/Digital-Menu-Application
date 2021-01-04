58034e6a89659845e8b58af0714fee178a403bcf
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2021-01-04 11:53:29 +0100

    Add menu image entity
    It is easier to store it in a seperate entity.

---
+++ backend/menu-service/src/main/java/com/dma/menuservice/model/MenuImage.java
@@ -0,0 +1,36 @@
+package com.dma.menuservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String type;
+
+    @Lob
+    private byte[] imageBytes;
+
+    public MenuImage(@NotNull String name, @NotNull String type, byte[] image) {
+        this.name = name;
+        this.type = type;
+        this.imageBytes = image;
+    }
+}
